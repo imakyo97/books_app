9304a58aabddbb37d0e6651cc259e86202a7babc
imakyo97 <kyoheishogun.1997@gmail.com>
2024-11-09 15:11:54 +0900

    BookのDBカラムにAuthorを追加し、新規作成と表示ができるよう修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -67,6 +67,6 @@ class BooksController < ApplicationController
     def book_params
       p "💩💩"
       p params
-      params.require(:book).permit(:title, :memo)
+      params.require(:book).permit(:title, :memo, :author)
     end
 end
